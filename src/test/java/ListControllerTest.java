6ec05cbcda5687829cc9b46c696c6ca6efe1de42
kayai <kayai.jia.login@outlook.com>
2022-05-04 18:05:16 +0800

    fix: 删除了多余文件

--- src/test/java/ListControllerTest.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;/**
- * 
- *
- * @author kayai
- * @date: 2022/4/30 13:50
- * @version 1.0
- */public class ListControllerTest {
-}
--- src/test/java/UserDAOTest.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;/**
- * 
- *
- * @author kayai
- * @date: 2022/4/23 21:10
- * @version 1.0
- */public class UserDAOTest {
-}
